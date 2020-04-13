e4c2a106f46d04afcffe06904c89ebac0e67c898
Dioris Diaz <diorid@gmail.com>
2020-04-13 06:41:44 -0400

    text centered

--- portfolioapp/src/components/landingPage.js
+++ portfolioapp/src/components/landingPage.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Landing extends Component {
     render(){
         return(
-            <div><h1>Landing Page</h1></div>
+            <div style={{width: '100%', margin:'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                            src="https://tec-sense.com/wp-content/uploads/2019/09/avtar-man.png"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+                       
+                        <hr/>
+                        <p> HTML/CSS | Bootstrap | Javascript | NodeJS | Express | React | MongoDB</p>
+                        <div className="social-links">
+                            <a href="https://www.linkedin.com/in/dioris-diaz-18a053191/" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-linkedin-square"  aria-hidden="true" />
+                            </a>
+                            <a href="https://github.com/Dioris-cpu" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-github-square"  aria-hidden="true" />
+                            </a>
+                        </div>
+                        </div>
+                    </Cell>
+                    
+                </Grid>
+
+            </div>
         )
     }
   
